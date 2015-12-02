4794469c8af10bbdb642d00b56624b85efcd29a0
fionazhang-owmessaging <fiona.zhang@owmessaging.com>
2015-12-02 18:22:51 +0800

    a

--- src/com/test/pageobject/Base.java
+++ src/com/test/pageobject/Base.java
@@ -1,30 +1,22 @@
 package com.test.pageobject;
 
+import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.ios.IOSDriver;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.support.PageFactory;
 
 public class Base {
 	DesiredCapabilities capability;
 	LoginPage loginPage;
+	IOSDriver<?> driver;
 //    public static InheritableThreadLocal<RemoteWebDriver> driver = new InheritableThreadLocal<RemoteWebDriver>();
-	public IOSDriver driver;
 	public Base() {
 		this.setSeleniumGrid();
-//		try {
-////			driver = new IOSDriver(new URL("http://10.13.16.37:44441/wd/hub"),capability);
-//
-//		} catch (MalformedURLException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
+		driver.get("http://172.26.201.178:8080/kiwi-octane");
 //		this.getDriver();
 //		driver.get().navigate().to("http://172.26.201.178:8080/kiwi-octane/index-tui.jsp");
 //		loginPage=PageFactory.initElements((IOSDriver)driver.get(), LoginPage.class);
@@ -42,13 +34,21 @@ public class Base {
 //        capability.setCapability("safariOpenLinksInBackground", "true");
         capability.setJavascriptEnabled(true);
         capability.setCapability("app",  "Safari");
-        
+    	 //driver;
+
+    	try {
+    		driver= new IOSDriver(new URL("http://10.13.16.37:44441/wd/hub"),capability);
+
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 
   	}
 	
-//    public WebDriver getDriver() {
-//    	IOSDriver webDriver = (IOSDriver)driver.get();
-//        return webDriver;
-//    }
+   /* public WebDriver getDriver() {
+    	IOSDriver webDriver = (IOSDriver)driver.get();
+        return webDriver;
+    }*/
 }
